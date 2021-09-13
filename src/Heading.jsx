e887921afd756d984bc0de956bbe08d2ac045f5e
R-Okje <68820812+R-Okje@users.noreply.github.com>
2021-09-13 12:37:59 +0530

    Initial commit

---
+++ src/Heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading() {
+  return <h1>This is my Heading.jsx</h1>;
+}
+export default Heading;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g
+//Created by Angela Yu.
+//Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+
+const myno = 8;
+const name = "Viral";
+var d = new Date();
+var n = d.getFullYear();
+var h = d.getHours();
+const customStyle = {
+  color: "green"
+};
+let greeting;
+if (h < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (h < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World,Its Viral here</h1>
+    <p>My lucky number is {myno}</p>
+    <p>This is created by {name}</p>
+    <p>Copyright {n} </p>
+    <p>Hours {h}</p>
+    <Heading />
+    <h1 style={customStyle}>{greeting}</h1>
+  </div>,
+  document.getElementById("root")
+);
